a5d11afdb29521a17ed7a12d0066dea2358bb8cb
avanitejani <avanitejani@gmail.com>
2024-02-07 21:54:24 +0530

    updatede

---
+++ jqury/prectice/pattren.js
@@ -0,0 +1,91 @@
+// let row = 10;
+// let col = 8;
+// let parth = "";
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// row 
+// for(i=1;i<=5;i++){
+// column 
+
+// for(j=1; j<=5;j++){
+
+//     parth+="*";
+
+// }
+// parth +="\n";
+// }
+// console.log(parth);
+
+// $$$$$
+// $$$$$
+// $$$$$
+// $$$$$
+// $$$$$ 
+
+
+
+// 11111
+// 11111
+// 11111 
+// 11111 
+// 11111
+
+
+
+// @
+// @@
+// @@@  
+// @@@@ 
+// @@@@@ 
+
+let Monika = " ";
+// row 
+for (i = 1; i <= 5; i++) {
+    // column 
+
+    for (j = 1; j <= i; j++) {
+
+        Monika += "@";
+
+    }
+    Monika += "\n";
+}
+console.log(Monika);
+
+// $$$$$
+// $$$$
+// $$$ 
+// $$   
+// $
+
+// let Vidhi = "";
+// // row 
+// for(i=1;i<=5;i++){
+// // column 
+
+// for(j=5; j>=i;j--){
+
+//     Vidhi+="@";
+
+// }
+// Vidhi +="\n";
+// }
+// console.log(Vidhi);
+
+
+let Vidhi = "";
+// row 
+for (i = 1; i <= 5; i++) {
+    // column 
+
+    for (j = i; j <= 5; j++) {
+
+        Vidhi += "@";
+
+    }
+    Vidhi += "\n";
+}
+console.log(Vidhi);

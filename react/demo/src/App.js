722c00ddaff359966169211dab5bb2844e8715f3
avanitejani <avanitejani@gmail.com>
2024-02-15 13:08:16 +0530

    updated

---
+++ react/demo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Head from './Head';
+
+function App() {
+  return (
+    <>
+      <Head />
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed esse iste illo reprehenderit alias commodi incidunt qui! Architecto, vitae. Dolorem temporibus accusantium iste, quibusdam recusandae alias vero cupiditate odit maiores.</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ react/demo/src/Head.js
@@ -0,0 +1,14 @@
+
+
+// rafce
+import React from 'react'
+
+const Head = () => {
+  return (
+      <>
+          <h1>hi this is header</h1>
+      </>
+  )
+}
+
+export default Head
